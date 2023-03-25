ad6e8721ced4b17ca06f532e8b0282c97e57c77c
waterlaw <abkerimov@gmail.com>
2023-03-25 23:49:52 +0300

    использовал UserMixin для flask_login

--- models/teacher_auth.py
+++ models/teacher_auth.py
@@ -1,22 +1,30 @@
-from sqlalchemy import Column, Integer, VARCHAR, ForeignKey
+import uuid
+
+from sqlalchemy import Column, VARCHAR, ForeignKey, Text, text
 from extensions.database_extension import base
 from sqlalchemy.dialects.postgresql import UUID
 
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
 
-class TeacherAuth(base):
+
+class TeacherAuth(base, UserMixin):
     __tablename__ = 'teacherAuth'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(VARCHAR(50))
-    password = Column(VARCHAR(250))
+    id = Column(UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()"))
+    email = Column(VARCHAR(100), nullable=False)
+    password_hash = Column(VARCHAR(255), nullable=False)
+    first_name = Column(VARCHAR(20), nullable=False)
+    middle_name = Column(VARCHAR(20), nullable=False)
+    last_name = Column(VARCHAR(20), nullable=False)
+    avatar = Column(Text)
 
-    teacher_id = Column(UUID(as_uuid=True), ForeignKey('teacher.id'))
-
-    def __init__(self, email, password, teacher_id):
+    def __init__(self, email, password, first_name, middle_name, last_name):
         self.email = email
-        self.password = password
-        self.teacher_id = teacher_id
+        self.password_hash = generate_password_hash(password)
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
 
     def __repr__(self):
-        return f'<Email: "{self.email}", TeacherId: "{self.teacher_id}">'
-
+        return f'<Email: "{self.email}", Teacher: "{self.first_name} {self.middle_name[0]}.{self.last_name[0]}.">'
