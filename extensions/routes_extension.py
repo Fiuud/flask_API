59d51ad865e274bf88d476ff7ff9de56b100aa44
waterlaw <abkerimov@gmail.com>
2023-03-25 23:38:47 +0300

    blueprint teacher_auth

--- extensions/routes_extension.py
+++ extensions/routes_extension.py
@@ -1,11 +1,13 @@
-from api.home import home_route
-from api.login import login_route
-from api.qr import qr_route
-from api.student import student_route
+from views.home import home_route
+from views.scanner_login import scanner_login_route
+from views.qr import qr_route
+from views.student import student_route
+from views.teacher_login import teacher_auth_route
 
 
 def register_routes(app):
     app.register_blueprint(student_route)
-    app.register_blueprint(login_route)
+    app.register_blueprint(scanner_login_route)
     app.register_blueprint(qr_route)
     app.register_blueprint(home_route)
+    app.register_blueprint(teacher_auth_route)
