91a0fdf8adb575b5e0055dfaef62601c478afc7d
unknown <abkerimov.e.i20@gmail.com>
2023-03-25 17:12:42 +0300

    Перенёс ициализацию jwt, turbo, init_db

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from flask import Flask
-
+from utils.jwt_token import jwt
+from utils.turbo import turbo
+from extensions.database_extension import init_db
 from config import Config
 from extensions.routes_extension import register_routes
 
@@ -8,7 +10,9 @@ def create_app():
     app = Flask(__name__, template_folder='templates')
     app.config.from_object(Config)
 
-
+    jwt.init_app(app)
+    turbo.init_app(app)
+    init_db(app)
 
     # will move to register_config soon
     # app.config['ERROR_404_HELP'] = False
--- run.py
+++ run.py
@@ -1,13 +1,6 @@
 from application import create_app
-from utils.jwt_token import jwt
-from utils.turbo import turbo
-from extensions.database_extension import init_db
 
 app = create_app()
-
-jwt.init_app(app)
-turbo.init_app(app)
-init_db(app)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000, )
